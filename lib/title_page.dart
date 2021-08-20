74798cd5cf28d25e8a97aba2398bd10908b48708
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-08-20 09:11:10 +0900

    4*4のまるばつゲームです。

--- lib/title_page.dart
+++ lib/title_page.dart
@@ -3,8 +3,9 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:marubatu_app/cpu_marubatu.dart';
 import 'package:marubatu_app/main.dart';
 
-
 class TitlePage extends StatelessWidget {
+  const TitlePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,8 +27,7 @@ class TitleHomePage extends StatefulWidget {
   _TitleHomePageState createState() => _TitleHomePageState();
 }
 
-class  _TitleHomePageState extends State<TitleHomePage> {
-
+class _TitleHomePageState extends State<TitleHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,39 +39,36 @@ class  _TitleHomePageState extends State<TitleHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-
             const Text('4×4', style: TextStyle(fontSize: 50)),
-
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                const Icon(FontAwesomeIcons.circle, size: 40, color: Colors.blue),
-                const Icon(Icons.clear, size: 45, color: Colors.red),
-                const Text('ゲーム', style: TextStyle(fontSize: 40)),
+              children: const [
+                Icon(FontAwesomeIcons.circle,
+                  size: 40,
+                  color: Colors.blue,
+                ),
+                Icon(Icons.clear, size: 45, color: Colors.red),
+                Text('ゲーム', style: TextStyle(fontSize: 40)),
               ],
             ),
-
-
             const SizedBox(height: 40),
-
-
             SizedBox(
               width: 280,
               height: 60,
               child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => MyHomePage(title: '')),
-                    );
-                  },
-                  child: const Text('友達と対戦',style: TextStyle(fontSize: 25),)
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MyHomePage()),
+                  );
+                },
+                child: const Text(
+                  '友達と対戦',
+                  style: TextStyle(fontSize: 25),
+                ),
               ),
             ),
             const SizedBox(height: 40),
-
-
             SizedBox(
               width: 280,
               height: 60,
@@ -81,16 +78,28 @@ class  _TitleHomePageState extends State<TitleHomePage> {
                     context: context,
                     builder: (_) {
                       return AlertDialog(
-                        title: Center(child: Text("現在開発中", style: TextStyle(
-                            fontSize: 20),)),
-                        content: Text("少々お待ちください",
-                          style: TextStyle(fontSize: 20),),
+                        title: const Center(
+                          child: Text(
+                            "現在開発中",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        content: const Text(
+                          "少々お待ちください",
+                          style: TextStyle(fontSize: 20),
+                        ),
                         actions: <Widget>[
                           // ボタン領域
-                          Spacer(),
+                          const Spacer(),
                           Center(
-                            child: FlatButton(
-                              child: const Text("閉じる", style: TextStyle(color: Colors.red,fontSize: 20)),
+                            child: TextButton(
+                              child: const Text(
+                                "閉じる",
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 20,
+                                ),
+                              ),
                               onPressed: () => Navigator.pop(context),
                             ),
                           ),
@@ -99,11 +108,12 @@ class  _TitleHomePageState extends State<TitleHomePage> {
                     },
                   );
                 },
-                child: const Text('CPUと対戦',style: TextStyle(fontSize: 25),),
+                child: const Text(
+                  'CPUと対戦',
+                  style: TextStyle(fontSize: 25),
+                ),
               ),
             ),
-
-
           ],
         ),
       ),
@@ -117,22 +127,32 @@ class  _TitleHomePageState extends State<TitleHomePage> {
         return AlertDialog(
           shape: const RoundedRectangleBorder(
               borderRadius: BorderRadius.all(Radius.circular(32.0))),
-          title: const Text('現在開発中！', textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 30),),
-          content: const Text('', textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 15),),
+          title: const Text(
+            '現在開発中！',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 30),
+          ),
+          content: const Text(
+            '',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 15),
+          ),
           actions: [
-            const Divider(color: Colors.black,),
+            const Divider(
+              color: Colors.black,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text('いいえ',
-                      style: TextStyle(color: Colors.blue, fontSize: 20,),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text(
+                    'いいえ',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20,
                     ),
                   ),
                 ),
@@ -150,7 +170,6 @@ class  _TitleHomePageState extends State<TitleHomePage> {
   }
 }
 //Navigator.push(context, MaterialPageRoute(builder: (context) => CPUPage()));
-
 
 /*
 return AlertDialog(
